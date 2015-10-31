21756b6b6c22e64200f2f4975b974e877c25a469
lemeshkob <lemeshkob@gmail.com>
2015-10-31 12:30:10 +0200

    commit

---
+++ courses/prog_base/tasks/self/kr/kr1/task6.c
@@ -0,0 +1,33 @@
+#include "stdlib.h"
+#include "stdio.h"
+#include "math.h"
+
+int coordinates()
+{
+	float xa, ya, za, xb, yb, zb, xc, yc, zc;
+	int AB,AC,BC;
+
+	AB = sqrt(pow((xb-xa),2) + pow((yb-ya),2) + pow((zb-za),2));
+	AC = sqrt(pow((xc-xa),2) + pow((yc-ya),2) + pow((zc-za),2));
+	BC = sqrt(pow((xc-xb),2) + pow((yc-yb),2) + pow((zc-zb),2));
+
+}
+
+int main()
+{
+	float xa, ya, za, xb, yb, zb, xc, yc, zc;
+
+	printf("%s\n","Enter x y z of A point" );
+	scanf("%f %f %f", &xa, &ya, &za);
+	printf("%s\n","Enter x y z of B point" );
+	scanf("%f %f %f", &xb, &yb, &zb);
+	printf("%s\n","Enter x y z of C point" );
+	scanf("%f %f %f", &xc, &yc, &zc);
+
+	struct points 
+	{
+		char pointA[3] = {xa, ya, za};
+		char pointB[3] = {xb, yb, zb};
+		char pointC[3] = {xc, yc, zc};
+	}
+}
