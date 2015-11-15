c432833ba3f1bdd706bee798d7235b2e2bdd208d
lemeshkob <lemeshkob@gmail.com>
2015-11-15 19:31:10 +0200

    yees

--- courses/prog_base/tasks/console/main.c
+++ courses/prog_base/tasks/console/main.c
@@ -1,30 +1,81 @@
-#include "stdlib.h"
-#include "stdio.h"
-#include "math.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <windows.h>
 
-#include "windows.h"
 
-int main()
+int main(void)
 {
-	int x = 0,y = 0;
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    COORD pos;
-    pos.X = x;
-    pos.Y = y;
+int aqua = BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE;
+int blue = BACKGROUND_BLUE | BACKGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE;
+int green = BACKGROUND_GREEN | BACKGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE;
 
-    for (y = 25; y >= 0 ; --y )
-    {	if(y%2 == 0 )
-    	{
-    		for(x = 79; x>=0; --x);
-    			printf("*");
-  		}
-    	else if(y&2 == 1)
-    	{
-    		for(x = 0; x >= 0; ++x)
-    		{
-    			printf("*");
-    		}
-    	}
-    }
-    return 0;
+
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+COORD pos;
+
+for(pos.Y = 24 ;pos.Y>=0 ; --pos.Y )
+{
+if(pos.Y%2==0)
+{
+for (pos.X = 79 ;pos.X>=0 ; --pos.X  )
+{
+SetConsoleCursorPosition(hConsole, pos);
+if((pos.X + pos.Y)%3 == 0 && (pos.X + pos.Y) <= 52){
+SetConsoleTextAttribute(hConsole, blue);
+}
+if((pos.X + pos.Y)%3 == 1 && (pos.X + pos.Y) <= 52){
+SetConsoleTextAttribute(hConsole, aqua);
+}
+if((pos.X + pos.Y)%3 == 2 && (pos.X + pos.Y) <= 52){
+SetConsoleTextAttribute(hConsole, green);
+}
+if((pos.X + pos.Y)%3 == 1 && (pos.X + pos.Y) > 52){
+SetConsoleTextAttribute(hConsole, blue);
+}
+if((pos.X + pos.Y)%3 == 0 && (pos.X + pos.Y) > 52){
+SetConsoleTextAttribute(hConsole, aqua);
+}
+if((pos.X + pos.Y)%3 == 2 && (pos.X + pos.Y) > 52){
+SetConsoleTextAttribute(hConsole, green);
+}
+
+puts("*");
+Sleep(10);
+}
+}
+if(pos.Y%2==1)
+{
+for (pos.X = 0;pos.X < 80 ; pos.X ++)
+{
+SetConsoleCursorPosition(hConsole, pos);
+
+if((pos.X + pos.Y)%3 == 0 && (pos.X + pos.Y) <= 52){
+SetConsoleTextAttribute(hConsole, blue);
+}
+if((pos.X + pos.Y)%3 == 1 && (pos.X + pos.Y) <= 52){
+SetConsoleTextAttribute(hConsole, aqua);
+}
+if((pos.X + pos.Y)%3 == 2 && (pos.X + pos.Y) <= 52){
+SetConsoleTextAttribute(hConsole, green);
+}
+if((pos.X + pos.Y)%3 == 1 && (pos.X + pos.Y) > 52){
+SetConsoleTextAttribute(hConsole, blue);
+}
+if((pos.X + pos.Y)%3 == 0 && (pos.X + pos.Y) > 52){
+SetConsoleTextAttribute(hConsole, aqua);
+}
+if((pos.X + pos.Y)%3 == 2 && (pos.X + pos.Y) > 52){
+SetConsoleTextAttribute(hConsole, green);
+}
+puts("*");
+Sleep(10);
+}
+}
+}
+
+
+pos.Y=25;
+pos.X=0;
+SetConsoleCursorPosition(hConsole, pos);
+
 }
