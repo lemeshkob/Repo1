c6f4eedd3da852745986d3af749f9e797ebf3d41
lemeshkob <lemeshkob@gmail.com>
2016-04-11 21:49:20 +0300

    Moving hero

--- courses/prog_base_3/project/CODE/AngryCreature/main.cpp
+++ courses/prog_base_3/project/CODE/AngryCreature/main.cpp
@@ -1,25 +1,63 @@
-
 #include <SFML/Graphics.hpp>
-#include "anim.hpp"
+#include <iostream>
 
 using namespace sf;
-
-
 int main()
 {
-    RenderWindow win(sf::VideoMode(200, 200), "SFML Test");
-    Texture q;
-    q.loadFromFile("rogue spritesheet calciumtrice.png");
+	RenderWindow window(sf::VideoMode(640, 480), "Lesson 7. kychka-pc.ru", Style::Fullscreen);
+
+    Image heroImage;
+    heroImage.loadFromFile("BOV_Warrior.png");
+    heroImage.createMaskFromColor(Color(255,163,177));
+
+	Texture herotexture;
+	herotexture.loadFromImage(heroImage);
+
+	Sprite herosprite;
+	herosprite.setTexture(herotexture);
+	herosprite.setTextureRect(IntRect(0, 71, 38, 67));
+	herosprite.setPosition(250, 250);
+
+	float CurrentFrame = 0;//хранит текущий кадр
+	Clock clock;
+
+	while (window.isOpen())
+	{
+
+		float time = clock.getElapsedTime().asMicroseconds();
+		clock.restart();
+		time = time / 800;
 
 
-    Animation anim;
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (Keyboard::isKeyPressed(Keyboard::Escape)){
+				window.close();
+			}
+		}
 
 
+		///////////////////////////////////////////Управление персонажем с анимацией////////////////////////////////////////////////////////////////////////
+		if ((Keyboard::isKeyPressed(Keyboard::Left))) { //если нажата клавиша стрелка влево или англ буква А
+			CurrentFrame += 0.005*time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
+			if (CurrentFrame < -8) CurrentFrame += 8; //проходимся по кадрам с первого по третий включительно. если пришли к третьему кадру - откидываемся назад.
+			herosprite.setTextureRect(IntRect(38 * int(CurrentFrame) + 38, 71, -38, 67)); //проходимся по координатам Х. получается 96,96*2,96*3 и опять 96
+			herosprite.move(-0.1*time, 0);//происходит само движение персонажа влево
+		}
 
-        win.clear(Color::White);
-        win.draw(heroSprite);
-        win.display();
-    }
+		if ((Keyboard::isKeyPressed(Keyboard::Right))) {
+			CurrentFrame += 0.005*time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
+			if (CurrentFrame > 8) CurrentFrame -= 8; //проходимся по кадрам с первого по третий включительно. если пришли к третьему кадру - откидываемся назад.
+			herosprite.setTextureRect(IntRect(38 * int(CurrentFrame), 71, 38, 67)); //проходимся по координатам Х. получается 96,96*2,96*3 и опять 96
 
-    return 0;
+			herosprite.move(0.1*time, 0);//происходит само движение персонажа вправо
+
+		}
+		window.clear();
+		window.draw(herosprite);
+		window.display();
+	}
+
+	return 0;
 }
